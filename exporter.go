c274c609d431f6569319bfa2e251849a59fce078
David Rio Deiros <driodeiros@gmail.com>
2022-07-07 09:00:44 -0400

    read csv

--- exporter.go
+++ exporter.go
@@ -1,10 +1,13 @@
 package ksak
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -58,8 +61,50 @@ func (l *ExporterCommand) Run() error {
 	gaugeLag.WithLabelValues("foo_bar", "gid_12", "localhost_9092").Add(123)
 	//gaugeLag.With(prometheus.Labels{"type": "delete", "user": "alice"}).Inc()
 
+	listEntries, err := readCsv()
+	if err != nil {
+		log.Fatalf("Could not load csv: %s", err)
+	}
+	fmt.Printf("%v ", listEntries)
+
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
 	http.Handle("/metrics", promhttp.Handler())
 	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", 8080), nil))
 	return nil
+}
+
+type csvEntry struct {
+	url     string
+	topic   string
+	groupId string
+}
+
+func readCsv() ([]csvEntry, error) {
+	file, err := os.Open("/dev/stdin")
+	if err != nil {
+		return nil, err
+	}
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	listEntries := []csvEntry{}
+	for _, entry := range records {
+		listEntries = append(listEntries, csvEntry{
+			url:     entry[0],
+			topic:   entry[1],
+			groupId: entry[2],
+		})
+	}
+
+	return listEntries, nil
 }
