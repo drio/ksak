70f7b00eee05b51225b5984dc8d5e3c9c57429e7
David Rio Deiros <driodeiros@gmail.com>
2022-07-08 16:11:45 -0400

    refactor list-topics with new kafka helper

---
+++ topics.go
@@ -0,0 +1,47 @@
+package ksak
+
+import (
+	"flag"
+	"fmt"
+)
+
+type ListTopicsCommand struct {
+	fs *flag.FlagSet
+
+	name string
+}
+
+func NewListTopicsCommand() *ListTopicsCommand {
+	gc := &ListTopicsCommand{
+		fs: flag.NewFlagSet("list-groups", flag.ContinueOnError),
+	}
+
+	return gc
+}
+
+func (l *ListTopicsCommand) Name() string {
+	return l.fs.Name()
+}
+
+func (l *ListTopicsCommand) Init(args []string) error {
+	return l.fs.Parse(args)
+}
+
+func (l *ListTopicsCommand) Run(kd *KafkaDetails) error {
+	kd.Init().SetPlainConn()
+	partitions, err := kd.Conn.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	m := map[string]struct{}{}
+
+	for _, p := range partitions {
+		m[p.Topic] = struct{}{}
+	}
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	return nil
+}
